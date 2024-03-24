b06ca6c6915acd81b9cc3287e61bc9ff669da989
Raphael Adjetey Adjei <100845372+RAAdjei@users.noreply.github.com>
2024-03-24 18:29:09 +0000

    Create Sum of Prefix Scores of Strings.py

---
+++ Sum of Prefix Scores of Strings.py
@@ -0,0 +1,31 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            node.count += 1
+
+    def prefix_count(self, word):
+        node = self.root
+        count = 0
+        for char in word:
+            count += node.children[char].count
+            node = node.children[char]
+        return count
+
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        return [trie.prefix_count(word) for word in words]
